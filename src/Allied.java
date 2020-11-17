8693dc68d30c33ca337a41aa3e2260ec676d75d4
SeyedNamiModarressi <s.namimodarressi@gmail.com>
2020-11-17 10:27:30 +0330

    Army Added

--- src/Allied.java
+++ src/Allied.java
@@ -1,2 +1,50 @@
-public class Allied {
+/**
+ * this is allied army
+ * @author Seyed Nami Modarressi
+ * @since 2020
+ * @version 1.0
+ */
+public class Allied extends Army{
+    /**
+     * creat allied army
+     */
+    public Allied(){
+        super();
+        firstArmy();
+    }
+
+    /**
+     * set first army
+     */
+    public void firstArmy(){
+        Element e1 = new Soldier(4,1,4);
+        Element e2 = new Soldier(4,6,4);
+        Element e3 = new Soldier(4,8,4);
+        Element e4 = new Soldier(4,11,4);
+        Element e5 = new Soldier(5,3,4);
+        Element e6 = new Soldier(6,7,4);
+        Element e7 = new Soldier(7,0,4);
+        Element e8 = new Artillery(7,1,2);
+        Element e9 = new Artillery(7,5,2);
+        Element e10 = new Soldier(7,9,4);
+        Element e11 = new Tank(8,0,3);
+        Element e12 = new Tank(8,1,3);
+        Element e13 = new Tank(8,8,3);
+        Element e14 = new Tank(8,12,3);
+        addElement(e1);
+        addElement(e2);
+        addElement(e3);
+        addElement(e4);
+        addElement(e5);
+        addElement(e6);
+        addElement(e7);
+        addElement(e8);
+        addElement(e9);
+        addElement(e10);
+        addElement(e11);
+        addElement(e12);
+        addElement(e13);
+        addElement(e14);
+    }
+
 }
--- src/Army.java
+++ src/Army.java
@@ -1,2 +1,95 @@
+/**
+ * in this class we can create new Army and interact with them
+ * @author Seyed Nami Modarressi
+ * @since 2020
+ * @version 1.0
+ */
+import java.util.ArrayList;
+
 public class Army {
+
+    private int score;
+    private ArrayList<Element> elements;
+
+    /**
+     * create new army
+     */
+    public Army() {
+        score = 0;
+        elements = new ArrayList<Element>();
+    }
+
+    /**
+     * @return score of army
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * @param score new score of army
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * @return elements of army
+     */
+    public ArrayList<Element> getElements() {
+        return elements;
+    }
+
+    /**
+     * update element location
+     *
+     * @param X previous location
+     * @param Y previous location
+     * @param x new location
+     * @param y new location
+     */
+    public void updateElementLocation(int X, int Y, int x, int y) {
+        for (Element sample : elements) {
+            if (sample.getX() == X && sample.getY() == Y) {
+                sample.setLocation(x, y);
+                break;
+            }
+        }
+    }
+
+    /**
+     * decrease counter of element
+     *
+     * @param x location of element
+     * @param y location of element
+     */
+    public void decreaseNumberOfElementsInLocation(int x, int y) {
+        for (Element sample : elements) {
+            if (sample.getX() == x && sample.getY() == y) {
+                sample.Decrease();
+                break;
+            }
+        }
+    }
+
+    /**
+     * add element to army
+     *
+     * @param e element that we want to add to army
+     */
+    public void addElement(Element e) {
+        elements.add(e);
+    }
+
+    /**
+     * max score is 6
+     *
+     * @return if army is winner or not
+     */
+    public boolean isWinner() {
+        if (getScore() == 6) {
+            return true;
+        }
+        return false;
+    }
 }
